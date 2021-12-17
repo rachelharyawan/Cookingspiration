f549b67fabac725af737e3c236167392847afd9b
rachelharyawan <62920741+rachelharyawan@users.noreply.github.com>
2021-12-18 01:06:42 +0700

    Add files via upload

--- Cookingspiration/app/src/main/java/com/application/cookingspiration/MainActivity.kt
+++ Cookingspiration/app/src/main/java/com/application/cookingspiration/MainActivity.kt
@@ -3,57 +3,119 @@ package com.application.cookingspiration
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import com.application.cookingspiration.details.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var firstdate: TextView
+    lateinit var seconddate: TextView
+    lateinit var thirddate: TextView
+    lateinit var fourthdate: TextView
+    lateinit var fifthdate: TextView
+    lateinit var sixthdate: TextView
+    lateinit var seventhdate: TextView
+    lateinit var date: TextView
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mondaycard: CardView = findViewById(R.id.mondaycard)
-        mondaycard.setOnClickListener {
-            val intent = Intent(this, MondayActivity::class.java)
+        val firstcard: CardView = findViewById(R.id.firstcard)
+        firstcard.setOnClickListener {
+            val intent = Intent(this, FirstCard::class.java)
             startActivity(intent)
         }
 
-        val tuesdaycard: CardView = findViewById(R.id.tuesdaycard)
-        tuesdaycard.setOnClickListener {
-            val intent = Intent(this, TuesdayActivity::class.java)
+        val secondcard: CardView = findViewById(R.id.secondcard)
+        secondcard.setOnClickListener {
+            val intent = Intent(this, SecondCard::class.java)
             startActivity(intent)
         }
 
-        val wednesdaycard: CardView = findViewById(R.id.wednesdaycard)
-        wednesdaycard.setOnClickListener {
-            val intent = Intent (this, WednesdayActivity::class.java)
+        val thirdcard: CardView = findViewById(R.id.thirdcard)
+        thirdcard.setOnClickListener {
+            val intent = Intent (this, ThirdCard::class.java)
             startActivity(intent)
         }
 
-        val thursdaycard: CardView = findViewById(R.id.thursdaycard)
-        thursdaycard.setOnClickListener {
-            val intent = Intent (this, ThursdayActivity::class.java)
+        val fourthcard: CardView = findViewById(R.id.fourthcard)
+        fourthcard.setOnClickListener {
+            val intent = Intent (this, FourthCard::class.java)
             startActivity(intent)
         }
 
-        val fridaycard: CardView = findViewById(R.id.fridaycard)
-        fridaycard.setOnClickListener {
-            val intent = Intent (this, FridayActivity::class.java)
+        val fifthcard: CardView = findViewById(R.id.fifthcard)
+        fifthcard.setOnClickListener {
+            val intent = Intent (this, FifthCard::class.java)
             startActivity(intent)
         }
 
-        val saturdaycard: CardView = findViewById(R.id.saturdaycard)
-        saturdaycard.setOnClickListener {
-            val intent = Intent (this, SaturdayActivity::class.java)
+        val sixthcard: CardView = findViewById(R.id.sixthcard)
+        sixthcard.setOnClickListener {
+            val intent = Intent (this, SixthCard::class.java)
             startActivity(intent)
         }
 
-        val sundaycard: CardView = findViewById(R.id.sundaycard)
-        sundaycard.setOnClickListener {
-            val intent = Intent (this, SundayActivity::class.java)
+        val seventhcard: CardView = findViewById(R.id.seventhcard)
+        seventhcard.setOnClickListener {
+            val intent = Intent (this, SeventhCard::class.java)
             startActivity(intent)
         }
+
+        firstdate = findViewById(R.id.firstdate)
+        seconddate = findViewById(R.id.seconddate)
+        thirddate = findViewById(R.id.thirddate)
+        fourthdate = findViewById(R.id.fourthdate)
+        fifthdate = findViewById(R.id.fifthdate)
+        sixthdate = findViewById(R.id.sixthdate)
+        seventhdate = findViewById(R.id.seventhdate)
+        date = findViewById(R.id.date)
+
+        val calendar : Calendar = Calendar.getInstance()
+        val first : Date = calendar.getTime()
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val second : Date = calendar.getTime()
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val third : Date = calendar.getTime()
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val fourth : Date = calendar.getTime()
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val fifth : Date = calendar.getTime()
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val sixth : Date = calendar.getTime()
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val seventh : Date = calendar.getTime()
+
+        val dateFormat : DateFormat = SimpleDateFormat("EEE, MMM d, yyyy")
+
+        val firstcarddate : String = dateFormat.format(first)
+        val secondcarddate : String = dateFormat.format(second)
+        val thirdcarddate : String = dateFormat.format(third)
+        val fourthcarddate : String = dateFormat.format(fourth)
+        val fifthcarddate : String = dateFormat.format(fifth)
+        val sixthcarddate : String = dateFormat.format(sixth)
+        val seventhcarddate : String = dateFormat.format(seventh)
+
+        firstdate.setText(firstcarddate)
+        seconddate.setText(secondcarddate)
+        thirddate.setText(thirdcarddate)
+        fourthdate.setText(fourthcarddate)
+        fifthdate.setText(fifthcarddate)
+        sixthdate.setText(sixthcarddate)
+        seventhdate.setText(seventhcarddate)
+        date.setText(firstcarddate + " - " + seventhcarddate)
+
     }
 
 }
--- Cookingspiration/app/src/main/java/com/application/cookingspiration/Startscreen.kt
+++ Cookingspiration/app/src/main/java/com/application/cookingspiration/Startscreen.kt
@@ -3,6 +3,7 @@ package com.application.cookingspiration
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
 
 class Startscreen : AppCompatActivity() {
